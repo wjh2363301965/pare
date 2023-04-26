df295876dd2ab56669642ba4c52258408fc4ec76
wangjh <wangjh@seclead.cn>
2023-04-26 14:48:57 +0800

    小恐龙跳跃障碍小游戏更新：1.随着跳跃数量变多而加快速度。                       2.修复跳的次数太多导致小恐龙悬浮问题

--- src/main/java/com/jianhaoweb/play/DinoGame.java
+++ src/main/java/com/jianhaoweb/play/DinoGame.java
@@ -6,12 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class DinoGame {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Dino Game");
+            JFrame frame = new JFrame(" ");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setContentPane(new GamePanel());
             frame.pack();
@@ -24,18 +25,22 @@ public class DinoGame {
     static class GamePanel extends JPanel implements ActionListener, KeyListener {
 
         private int dinoX = 50;
-        private int dinoY = 100;
-        private int dinoWidth = 20;
+        private int dinoY = 6;
+        private int dinoWidth = 5;
         private int dinoHeight = 20;
         private int obstacleX = 250;
-        private int obstacleY = 100;
-        private int obstacleWidth = 20;
+        private int obstacleY = 6;
+        private int obstacleWidth = 5;
         private int obstacleHeight = 30;
         private int score = 0;
         private boolean isJumping = false;
         private int jumpHeight = 50;
         private int jumpStep = 0;
         private Timer timer;
+        private JButton restartButton;
+        private Random random;
+        private int obstacleSpeed = 3;
+
 
         public GamePanel() {
             setPreferredSize(new Dimension(300, 150));
@@ -44,55 +49,92 @@ public class DinoGame {
             addKeyListener(this);
             timer = new Timer(20, this);
             timer.start();
+            restartButton = new JButton(".");
+            restartButton.addActionListener(e -> restartGame());
+            restartButton.setVisible(false);
+            add(restartButton);
+
+            random = new Random();
         }
 
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
 
+            Graphics2D g2d = (Graphics2D) g.create();
+
+            // 将坐标系上下翻转，使得y轴正方向朝上
+            g2d.scale(1, -1);
+            // 平移坐标系，使原点位于左下角
+            g2d.translate(0, -getHeight());
+
+            // 在这里添加你的绘制代码，使用g2d对象
+            // 例如：g2d.drawRect(10, 10, 50, 50);
+
+
+
             // 绘制恐龙
-            g.setColor(Color.LIGHT_GRAY);
+            /*g.setColor(Color.LIGHT_GRAY);
 
-            g.fillRect(dinoX, dinoY, dinoWidth, dinoHeight);
-
+            g.fillRect(dinoX, dinoY, dinoWidth, dinoHeight);*/
+            g2d.setColor(Color.LIGHT_GRAY);
+            g2d.fillRect(dinoX, dinoY, dinoWidth, dinoHeight);
             // 绘制障碍物
-            g.setColor(Color.LIGHT_GRAY);
-            g.fillRect(obstacleX, obstacleY, obstacleWidth, obstacleHeight);
-
+          /*  g.setColor(Color.LIGHT_GRAY);
+            g.fillRect(obstacleX, obstacleY, obstacleWidth, obstacleHeight);*/
+            g2d.setColor(Color.LIGHT_GRAY);
+            g2d.fillRect(obstacleX, obstacleY, obstacleWidth, obstacleHeight);
             // 绘制得分
+          /*  g.setColor(Color.LIGHT_GRAY);
+            g.drawString("Score: " + score, 10, 20);*/
+
             g.setColor(Color.LIGHT_GRAY);
             g.drawString("Score: " + score, 10, 20);
+            g2d.dispose();
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
             // 更新障碍物位置
-            obstacleX -= 3;
+            obstacleX -=  obstacleSpeed;
             if (obstacleX < -obstacleWidth) {
                 obstacleX = 250;
+                obstacleY = 6;
+                obstacleHeight = 30 + random.nextInt(30);
                 score++; // 恐龙跳过障碍物时加一分
+                // 每过5分，增加障碍物速度.当速度==一定数后，不加速了
+                if (score % 5 == 0 && obstacleSpeed <30) {
+                    obstacleSpeed++;
+                }
             }
 
             // 更新恐龙跳跃
             if (isJumping) {
                 if (jumpStep <= jumpHeight) {
-                    dinoY -= 2;
+                    dinoY += 2;
                     jumpStep += 2;
                 } else if (jumpStep < jumpHeight * 2) {
-                    dinoY += 2;
+                    dinoY -= 2;
                     jumpStep += 2;
                 } else {
                     isJumping = false;
                     jumpStep = 0;
+                }
+                // 如果跳跃结束后，恐龙的位置低于起始高度，将其设置为起始高度
+                if (!isJumping && dinoY > 6) {
+                    dinoY = 6;
                 }
             }
 
             // 检测碰撞
             if (new Rectangle(dinoX, dinoY, dinoWidth, dinoHeight).intersects(new Rectangle(obstacleX, obstacleY, obstacleWidth, obstacleHeight))) {
                 timer.stop(); // 恐龙碰到障碍物，游戏结束
+                restartButton.setVisible(true);
             }
 
             repaint();
+
+
         }
 
         @Override
@@ -108,5 +150,16 @@ public class DinoGame {
         @Override
         public void keyReleased(KeyEvent e) {}
 
+        private void restartGame() {
+            dinoY = 6;
+            obstacleX = 250;
+            obstacleY = 6 ;
+            score = 0;
+            isJumping = false;
+            jumpStep = 0;
+            obstacleSpeed = 3;
+            restartButton.setVisible(false);
+            timer.restart();
+        }
     }
 }
